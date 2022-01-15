2f5fd983a540394a63c993acef53ecec3e8807f8
Pablito Verde <pgreeny@mail.ru>
2022-01-15 19:05:53 +0300

    Урок 2
    Задание 1 урока 2

---
+++ Lesson2/Lesson2/BankAccount.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип). 
+Предусмотреть методы для доступа к данным – заполнения и чтения. Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать.
+*/
+
+namespace Lesson2
+{
+    public enum TypeAccount
+    {
+        deposit,
+        current,
+        credit
+    }
+
+    public class BankAccount
+    {
+        private int accountNumber;
+        private double balance;
+        private TypeAccount typeAccount;
+
+        public void AddAccountNumber(int accountNumber)
+        {
+            this.accountNumber = accountNumber;
+        }
+
+        public int GetAccountNumber()
+        {
+            return this.accountNumber;
+        }
+        public void AddBalance(double sum)
+        {
+            balance += sum;
+        }
+
+        public double GetAccountBalance()
+        {
+            return this.balance;
+        }
+
+        public void ChangeTypeAccount(TypeAccount typeAccount)
+        {
+            this.typeAccount = typeAccount;
+        }
+
+        public string GetTypeAccount()
+        {
+            if (typeAccount == TypeAccount.deposit)
+                return "Депозитный";
+            if (typeAccount == TypeAccount.current)
+                return "Расчетный";
+            if (typeAccount == TypeAccount.credit)
+                return "Кредитный";
+            else
+                return "Тип счета не определен";
+        }
+
+    }
+}
---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа банковских счетов");
+
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("Введите номер счета");
+            ba.AddAccountNumber(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Введите баланс");
+            ba.AddBalance(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Введите тип счета: 1. Депозитный 2. Расчетный 3. Кредитный");
+            int i = Convert.ToInt32(Console.ReadLine());
+            if (i == 1)
+            {
+                TypeAccount ta = TypeAccount.deposit;
+                ba.ChangeTypeAccount(ta);
+            }
+            else if (i == 2)
+            {
+                TypeAccount ta = TypeAccount.current;
+                ba.ChangeTypeAccount(ta);
+            }
+            else if (i == 3)
+            {
+                TypeAccount ta = TypeAccount.credit;
+                ba.ChangeTypeAccount(ta);
+            }
+
+            Console.WriteLine($"Новый счет: {ba.GetAccountNumber()}. Состояние баланса: {ba.GetAccountBalance()}. Тип счета: {ba.GetTypeAccount()}");
+
+            Console.ReadKey();
+        }
+    }
+}
