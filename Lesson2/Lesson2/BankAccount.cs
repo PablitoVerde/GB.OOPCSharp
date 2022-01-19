d4f7315c19719d7101d52bd7003fd5f2879f5d61
Pablito Verde <pgreeny@mail.ru>
2022-01-19 21:24:28 +0300

    Урок 3
    Домашнее задание к уроку 3.

--- Lesson2/Lesson2/BankAccount.cs
+++ Lesson2/Lesson2/BankAccount.cs
@@ -20,6 +20,11 @@ using System.Text;
 возможно ли снять запрашиваемую сумму, и в случае положительного результата изменяет баланс.
 */
 
+/* Урок 3
+ * 1. В класс банковский счет, созданный в упражнениях, добавить метод, который переводит деньги с одного счета на другой. 
+ * У метода два параметра: ссылка на объект класса банковский счет откуда снимаются деньги, второй параметр – сумма.
+ */
+
 
 namespace Lesson2
 {
@@ -65,6 +70,10 @@ namespace Lesson2
             return AccountNumber;
         }
 
+        /// <summary>
+        /// Добавить баланс на счет
+        /// </summary>
+        /// <param Сумма="sum"></param>
         public void AddBalance(double sum)
         {
             double balance = Balance;
@@ -72,6 +81,10 @@ namespace Lesson2
             Balance = balance;
         }
 
+        /// <summary>
+        /// Снять деньги со счета
+        /// </summary>
+        /// <param Сумма="sum"></param>
         public void WithdrawFromBalance(double sum)
         {
             if (Balance > sum)
@@ -88,6 +101,9 @@ namespace Lesson2
         public double Balance { get; set; }
         public TypeAccount TypeAccount { get; set; }
 
+        /// <summary>
+        /// Метод по выводу сведений о счете на экран
+        /// </summary>
         public void PrintAccountInfo()
         {
             string _typeAccount;
@@ -102,6 +118,24 @@ namespace Lesson2
                 _typeAccount = "Тип счета не определен";
 
             Console.WriteLine($"Новый счет: {ClientAccountNumber}. Состояние баланса: {Balance}. Тип счета: {_typeAccount}");
+        }
+
+        /// <summary>
+        /// Метод по переводу денег со счета. 
+        /// </summary>
+        /// <param Источник перевода="bankAccountSource"></param>
+        /// <param Сумма="sum"></param>
+        public void MakeTransaction (BankAccount bankAccountSource, double sum)
+        {
+            if (bankAccountSource.Balance>=sum)
+            {
+                bankAccountSource.Balance -= sum;
+                Balance += sum;
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств на счете. Операция не произведена");
+            }
         }
 
     }
---
+++ Lesson3/Lesson3-Task3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+/*
+ * 3. * Работа со строками. Дан текстовый файл, содержащий ФИО и e-mail адрес. Разделителем между ФИО и адресом электронной почты является символ &: 
+ * Кучма Андрей Витальевич & Kuchma@mail.ru Мизинцев Павел Николаевич & Pasha@mail.ru Сформировать новый файл, содержащий список адресов электронной почты. 
+ * Предусмотреть метод, выделяющий из строки адрес почты. Методу в качестве параметра передается символьная строка s, e-mail возвращается в той же строке s: 
+ * public void SearchMail (ref string s).
+ * */
+
+namespace Lesson3_Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Имена и адреса");
+
+            string path = Directory.GetCurrentDirectory() + @"\FIO.txt";
+
+            GetEmails(path);
+        }
+
+        static void GetEmails(string path)
+        {
+            //путь для записи списка адресов
+            string path2 = Directory.GetCurrentDirectory() + @"\emails.txt";
+
+            // проверка пути с именами и адресами
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Файл с данными не найдем", path);
+
+            //чтение файла построчно и запись в другой файл
+            FileStream fs1 = new FileStream(path, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs1);
+
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+                SearchMail(ref line);
+                File.AppendAllText(path2, line + Environment.NewLine);
+            }
+        }
+
+        static void SearchMail(ref string s)
+        {
+            s = s.Trim();
+            s.IndexOf('&');
+            s = s.Substring(s.IndexOf('&'));
+            s = s.Remove(0, 1);
+            s = s.Trim();
+        }
+    }
+}
