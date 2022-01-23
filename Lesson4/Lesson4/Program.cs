b8e1ebf577533c8bebdb68cf031f9209f9931ed3
Pablito Verde <pgreeny@mail.ru>
2022-01-23 12:32:40 +0300

    Урок 4
    Домашнее задание урок 4.

---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Building b = new Building(50, 5, 60, 3);
+            Console.WriteLine($"ID {b.Id}. Высота {b.Height}. Этажи {b.FloorCount}. Квартиры {b.FlatCount}.  Подъезды {b.EntranceCount}");
+            Console.WriteLine($"Квартиры в подъезде {b.flatOnBlock()}. Квартир на этаже {b.flatOnFloor()}. Высота потолка {b.floorHeight()}");
+
+            Console.ReadKey();
+        }
+    }
+}
